a145b516a5b1b6167e2244159387d576856c3dda
JericFederer <pianocoderodyssey@gmail.com>
2024-11-20 12:13:46 +0900

    details screen updated and slider added

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,13 +1,16 @@
 import { Link, router, useLocalSearchParams } from 'expo-router';
 import { Text, View, Pressable } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Slider from '@react-native-community/slider';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { meditations } from '@/data';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function MeditationDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const { top, bottom } = useSafeAreaInsets();
   const meditation = meditations.find((m) => m.id === Number(id));
 
   if (!meditation) {
@@ -15,30 +18,18 @@ export default function MeditationDetails() {
   }
 
   return (
-    <SafeAreaView className="bg-blue-400 flex-1 p-2 justify-between">
+    <SafeAreaView className="bg-sky-600 flex-1 p-2 justify-between">
       {/* Page content */}
       <View className="flex-1">
         {/* Top part of the screen */}
-        <View className="flex-1">
+        <View className="bg-sky-300 flex-1">
           {/* Header */}
           <View className="flex-row items-center justify-between p-10">
             <AntDesign name="infocirlceo" size={24} color="black" />
 
             <View className="bg-zinc-800 p-2 rounded-md">
               <Text className="text-zinc-100 font-semibold">
-                Today's meditation
-              </Text>
-            </View>
-
-            <View className="bg-zinc-800 p-2 rounded-md">
-              <Text className="text-zinc-100 font-semibold">
-                Top: {top}
-              </Text>
-            </View>
-
-            <View className="bg-zinc-800 p-2 rounded-md">
-              <Text className="text-zinc-100 font-semibold">
-                Bottom: {bottom}
+                Today's Meditation
               </Text>
             </View>
 
@@ -50,12 +41,68 @@ export default function MeditationDetails() {
             />
           </View>
 
-          <Text className="text-3xl mt-20 text-center text-zinc-800 font-semibold">
+          <Text className="text-3xl text-center text-zinc-800 font-bold">
             {meditation?.title}
           </Text>
         </View>
 
+        {/* Middle part of the screen */}
+        <View className="bg-sky-500 flex-1 justify-center items-center">
+          {/* Play/Pause Button */}
+          {/* <Pressable
+            onPress={() => (player.playing ? player.pause() : player.play())}
+            className="bg-zinc-800 self-center w-20 aspect-square rounded-full items-center justify-center"
+          >
+            <FontAwesome6
+              name={status.playing ? 'pause' : 'play'}
+              size={24}
+              color="snow"
+            />
+          </Pressable> */}
+          <FontAwesome6
+              name='play'
+              size={24}
+              color="snow"
+            />
+        </View>
+
+        {/* Bottom part of the screen */}
+        <View className="flex-1">
+          {/* Footer: Player */}
+          <View className="p-5 mt-auto gap-5">
+            <View className="flex-row justify-between">
+              <MaterialIcons name="airplay" size={24} color="black" />
+              <MaterialCommunityIcons name="cog-outline" size={24} color="black" />
+            </View>
+
+            {/* Playback Indicator */}
+            <View>
+              {/* <View className="bg-blue-400 h-2" /> */}
+              <Slider
+                style={{width: "100%", height: 40}}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#7dd4fc"
+                maximumTrackTintColor="#000000"
+                thumbTintColor="#7dd4fc"
+              />
+            </View>
+
+            {/* Timer */}
+            <View className="flex-row justify-between">
+              <Text className="text-sky-100">
+                1:11
+              </Text>
+              <Text className="text-sky-100">
+                11:11
+              </Text>
+            </View>
+
+          </View>
+          
+        </View>
       </View>
+      
     </SafeAreaView>
   );
 }
